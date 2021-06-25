920825432fc28497c2bab454efdbf8a9f666de98
Xolmat <xolmatrw@gmail.com>
2021-06-25 15:28:15 +0500

    25.06.2021 15:28:15,83

--- app/backend/system/console/Console.php
+++ app/backend/system/console/Console.php
@@ -4,18 +4,38 @@ namespace App\System\Console;
 
 class Console
 {
-
+    /**
+     * @var
+     * STDIN = fopen("php://stdin", "r");
+     * STDOUT = fopen("php://stdout", "w");
+     * STDERR = fopen("php://stderr", "w");
+     */
     protected $args = null;
+    protected $ini = null;
 
     public function __construct()
     {
 
     }
 
-
     public function getArgs()
     {
         global $argv;
         $this->args = $argv;
     }
+
+    public function normilize()
+    {
+        // console argumetn escaping
+        $escape = "\\";
+        $oneQuote = "\\";
+    }
+
+    public static function isCli()
+    {
+        if (php_sapi_name() === 'cli')
+            return true;
+        return false;
+    }
+
 }
